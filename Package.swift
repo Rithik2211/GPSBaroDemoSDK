d883bb0f84efb98f3deea573ef4c9d2f418c4516
Rithik2211 <rithikpranao22@gmail.com>
2024-10-04 00:22:18 +0530

    "Updated Package.swift to specify iOS 15 platform and modified LocationViewModel class to make properties and methods public."

--- Package.swift
+++ Package.swift
@@ -5,15 +5,15 @@ import PackageDescription
 
 let package = Package(
     name: "GPSBaroDemoSDK",
+    platforms: [
+        .iOS(.v15)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "GPSBaroDemoSDK",
             targets: ["GPSBaroDemoSDK"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "GPSBaroDemoSDK"),
         .testTarget(
